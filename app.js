58091c2fa4850d4f2248d0e2afcff3542cfbbc25
Yvngscar <oscarmwas65@gmail.com>
2023-02-10 21:00:23 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//variables
+
+let openBtn = document.getElementById("open-btn");
+let modalContainer = document.getElementById("modal-container");
+let closeBtn = document.getElementById("close-btn");
+
+//event listeners
+
+openBtn.addEventListener("click", function () {
+  modalContainer.style.display = "block";
+});
+
+closeBtn.addEventListener("click", function () {
+  modalContainer.style.display = "none";
+});
+
+window.addEventListener("click", function (e) {
+  if (e.target === modalContainer) {
+    modalContainer.style.display = "none";
+  }
+});
